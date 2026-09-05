f12c19fe94fcc386920ff3c972702a433013342c
Ortamy <hamaschiah@proton.me>
2026-09-06 00:09:05 +0300

    video: удалить шаблонную композицию MyComp

--- products/video/src/Root.tsx
+++ products/video/src/Root.tsx
@@ -1,12 +1,10 @@
 import "./index.css";
-import { MyComposition } from "./Composition";
 import { Composition } from "remotion";
 import { WordOfTheDay } from "./WordOfTheDay";
 
 export const RemotionRoot: React.FC = () => {
   return (
     <>
-      <MyComposition />
       <Composition id="WordOfTheDay" component={WordOfTheDay} durationInFrames={450} fps={30} width={1920} height={1080} />
     </>
   );
